fb9f1a1d5d918a071fe0f38912c07eec688dc431
Recolic Keghart <root@recolic.net>
2021-10-16 19:56:54 +0800

    improve comments

--- background.js
+++ background.js
@@ -103,7 +103,11 @@ function storeInBrowserStorage(item, callback_function) {
  * If key already exists, it modify the value. 
  * If key doesn't exist, it add the key-value pair. 
  * If value is undefined, it delete the key-value pair from cookies. 
+ *
  * Assuming that, the same key SHOULD NOT appear twice in cookies. 
+ * Also assuming that, all cookies doesn't contains semicolon. 
+ *   (99.9% websites are following these rules)
+ *
  * Recolic K <root@recolic.net>
  */
 function cookie_keyvalues_set(original_cookies, key, value) {
